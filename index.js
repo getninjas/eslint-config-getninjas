4f106459b4ade6838efcebf1b12c9a71cdd9074f
Eduardo Matos <eduardoj.matos@gmail.com>
2017-06-03 09:49:37 -0300

    Index and initial package.json

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: "airbnb-base",
+
+  parser: "babel-eslint",
+
+  plugins: [
+    "babel"
+  ],
+
+  env: {
+    browser: true,
+    es6: true,
+    jasmine: true,
+    jquery: true
+  },
+
+  globals: {
+    context: false,
+    appendLoadFixtures: false,
+    readFixtures: false,
+    loadFixtures: false,
+    modulejs: false,
+    preloadFixtures: false,
+    spyOnEvent: false
+  },
+
+  rules: {
+    strict: 0,
+    func-names: 0
+  }
+}
