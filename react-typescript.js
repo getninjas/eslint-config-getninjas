1891926d9b611aab35d104345c4b2f2932f2143b
Danilo Trevelin <danilo.muller.trevelin@gmail.com>
2023-09-29 15:40:59 -0300

    feat(react-typescript): apply rules

--- react-typescript.js
+++ react-typescript.js
@@ -1,64 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-    jest: true,
-    node: true
-  },
   extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:jsx-a11y/recommended', // Accessibility rules
-    'plugin:prettier/recommended' // Prettier recommended rules
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
+    './rules/typescript',
+    './rules/react'
+  ].map(require.resolve),
   rules: {
-    'import/no-anonymous-default-export': 'off',
-    'comma-dangle': 0,
-    '@typescript-eslint/dot-notation': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    'global-require': 'off',
-    'no-param-reassign': 'off',
-    'no-underscore-dangle': 'off',
-    'camelcase': 'off',
-    'no-console': ['error', { 'allow': ['tron'] }],
-    'import/no-extraneous-dependencies': 'off',
-    'no-use-before-define': ['error', 'nofunc'],
-    '@typescript-eslint/no-use-before-define': ['error'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/space-before-function-paren': 'off',
-    '@typescript-eslint/semi': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    'import/no-duplicates': 'off',
-    'multiline-ternary': 'off',
-    'react/button-has-type': 'off',
-    'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
-    'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
-    'react/jsx-no-bind': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    "react/jsx-filename-extension": [1, { 
-      "extensions": [".js", ".jsx", ".ts", ".tsx"] 
-    }]
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        'extensions': ['.ts', '.tsx']
+      }
+    ]
+  }
 }
