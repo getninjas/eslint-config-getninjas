e65c662acacfba5cb06cb259e40033dead8de065
Henrique Tavares <ihenrits@gmail.com>
2023-09-29 12:21:33 -0300

    chore: config eslint from react and react-ts

--- react-typescript.js
+++ react-typescript.js
@@ -1,19 +1,20 @@
 module.exports = {
   env: {
-    "browser": true,
-    "es2020": true,
-    "jest": true,
-    "node": true
+    browser: true,
+    es2022: true,
+    jest: true,
+    node: true
   },
   extends: [
+    'standard-with-typescript',
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'plugin:prettier/recommended' // Prettier recommended rules
   ],
-  overrides: [
-  ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
+    project: 'tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
@@ -21,33 +22,36 @@ module.exports = {
     'react'
   ],
   rules: {
-    "@next/next/no-page-custom-font": "off",
-    "import/no-anonymous-default-export": "off",
+    'import/no-anonymous-default-export': 'off',
     'comma-dangle': 0,
-    "prettier/prettier": "error",
-    "@typescript-eslint/dot-notation": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/member-delimiter-style": "off",
-    "import/prefer-default-export": "off",
-    "import/extensions": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    "global-require": "off",
-    "no-param-reassign": "off",
-    "no-underscore-dangle": "off",
-    "camelcase": "off",
-    "no-console": ["error", { "allow": ["tron"] }],
-    "import/no-extraneous-dependencies": "off",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/space-before-function-paren": "off",
-    "@typescript-eslint/semi": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/prefer-nullish-coalescing": "off",
-    "import/no-duplicates": "off",
-    "multiline-ternary": "off"
+    '@typescript-eslint/dot-notation': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'global-require': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'camelcase': 'off',
+    'no-console': ['error', { 'allow': ['tron'] }],
+    'import/no-extraneous-dependencies': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'import/no-duplicates': 'off',
+    'multiline-ternary': 'off',
+    'react/button-has-type': 'off',
+    'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
+    'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
+    'react/jsx-no-bind': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
   settings: {
     react: {
--- react.js
+++ react.js
@@ -1,14 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2022: true,
     jest: true,
+    node: true,
   },
   extends: [
+    'airbnb',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:prettier/recommended', // Prettier recommended rules
-    'airbnb',
+    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'plugin:prettier/recommended' // Prettier recommended rules
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -22,12 +24,10 @@ module.exports = {
     'react/button-has-type': 'off',
     'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
     'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'no-use-before-define': ['error', 'nofunc'],
+    'no-use-before-define': ['error'],
     'react/jsx-no-bind': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'no-underscore-dangle': ['error', { allow: ['_links'] }],
+    'no-underscore-dangle': "off",
     'comma-dangle': 0,
   },
 };
