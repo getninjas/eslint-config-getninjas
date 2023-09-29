8045bec1fbb517f4f6882a8a78e39892cc9c8b42
Danilo Trevelin <danilo.muller.trevelin@gmail.com>
2023-09-29 15:40:46 -0300

    feat(react): apply rules

--- react.js
+++ react.js
@@ -1,36 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-    jest: true,
-    node: true,
-  },
   extends: [
     'airbnb',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:jsx-a11y/recommended', // Accessibility rules
-    'plugin:prettier/recommended' // Prettier recommended rules
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
+    './rules/react'
+  ].map(require.resolve),
   rules: {
-    'import/prefer-default-export': 'off',
-    'react/button-has-type': 'off',
-    'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
-    'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
-    'no-use-before-define': ['error', 'nofunc'],
-    'react/jsx-no-bind': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'no-underscore-dangle': "off",
-    'comma-dangle': 0,
-    "react/jsx-filename-extension": [1, { 
-      "extensions": [".js", ".jsx", ".ts", ".tsx"] 
-    }]
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        'extensions': ['.js', '.jsx']
+      }
+    ]
   },
 };
