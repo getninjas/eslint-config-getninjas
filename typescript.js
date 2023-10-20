d1a88766e3f7667ff7efd78b0b4c114f08a248a2
Danilo Trevelin <danilo.muller.trevelin@gmail.com>
2023-10-20 17:45:25 -0300

    feat(typescript): add prettier config

--- typescript.js
+++ typescript.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: [
     'eslint-config-airbnb-typescript/base',
     './rules/base.js',
-    './rules/typescript.js'
+    './rules/typescript.js',
+    'prettier',
   ].map(require.resolve),
   rules: {}
 }
