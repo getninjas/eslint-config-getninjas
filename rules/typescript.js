433706c9bf3458470c332f25402c44e3d00331c9
Henrique Tavares <ihenrits@gmail.com>
2023-10-20 17:05:16 -0300

    add editor config and peerDependencies

--- rules/typescript.js
+++ rules/typescript.js
@@ -5,7 +5,7 @@ module.exports = {
     // Precisamos do extend mesmo?
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:import/typescript'
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,7 +15,8 @@ module.exports = {
     // We deactivated the rule to use the typescript equivalent
     // with the same configurations used in our base file
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': baseRules['no-use-before-define'],
+    '@typescript-eslint/no-use-before-define':
+      baseRules['no-use-before-define'],
 
     // We deactivated the rule to use the typescript equivalent
     // with the same configurations used in our base file
@@ -25,5 +26,14 @@ module.exports = {
     // This rule is deactivated because it is stylistic, so prettier will
     // handle this responsability.
     '@typescript-eslint/member-delimiter-style': 'off',
+
+    // Add 'type' before interface or type in imports
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
   },
 };
